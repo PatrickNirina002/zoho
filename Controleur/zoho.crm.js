bef3c9f63249bc33017a0833695ca7956937cc4c
PatrickNirina002 <pjsranaivo@gmail.com>
2019-07-10 09:56:40 +0300

    zoho

---
+++ Controleur/zoho.crm.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "client_id": "1000.JWC48MO5N4ST01580WRV01OIHPIK7H",
+    "client_secret": "798c4f9daa85b7d1d1d45b97e39d9ddf74a5409363",
+    "redirect_url": "http://localhost:8070/profil",
+    "refresh_token": "[required] use the refresh token you got in exchange for the grant token",
+    "iamurl": "[optional] e.g. accounts.zoho.com or accounts.zoho.eu",
+    "base_url": "[optional]",
+    "user_identifier": "pjsranaivo@gmail.com",
+    "mysql_module": "local",
+    "mysql_username": "root",
+    "mysql_password": ""
+  };
---
+++ Route/route.js
@@ -0,0 +1,11 @@
+module.exports = (app) => {
+
+  const pers = require('../Controleur/controle');
+  app.post('/profil', pers.PosteComment);
+  // app.get('/profil', pers.findAll);
+   app.get('/profil/:profilId', pers.findOne);
+  // app.get('/user/:photo_profil', pers.lireImage);
+  // // app.put('/profil/:profilId', profil.update);
+  // // app.delete('/profil/:profilId', profil.delete);
+  
+}
